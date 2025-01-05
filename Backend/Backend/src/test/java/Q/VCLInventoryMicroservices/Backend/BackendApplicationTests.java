8c99ed5522047bbbfe9590e8864ab9688284b560
QVCL <quezada.venegas.carloivan@gmail.com>
2025-01-04 19:39:36 -0600

    add Primeros cambios

---
+++ Backend/Backend/src/test/java/Q/VCLInventoryMicroservices/Backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package Q.VCLInventoryMicroservices.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
